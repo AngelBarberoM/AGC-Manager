95e1f42a3de736da2af31de9d3f84459067ec786
Ángel Barbero Mellado <i82bamea@uco.es>
2023-12-18 17:11:55 +0100

    Funcionalidad Editar Campos User

--- src/public/js/user.js
+++ src/public/js/user.js
@@ -13,38 +13,283 @@ fetch(`/users/details/${userId}`)
     } else {
       const row = document.createElement('tr')
 
+      // USERNAME
+
       const usernameCell = document.createElement('td')
       usernameCell.textContent = data.username
+
+      // Botón de Actualizar Username
+      const updateUsernameButton = document.createElement('img')
+
+      updateUsernameButton.src = '/img/editar.png'
+      updateUsernameButton.alt = 'Actualizar NombreUsuario'
+      updateUsernameButton.className = 'chiquito separado'
+
+      // Funcionalidad Botón Actualizar
+      updateUsernameButton.addEventListener('click', () => {
+        const updateForm = document.createElement('form')
+        const updateFormContainer = document.querySelector('.updateFormContainer')
+
+        updateForm.innerHTML = `
+            <table>
+            <thead>
+              <tr>
+                <th>Nombre de Usuario</th>
+                <th></th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <td>
+                  <label for="username">Nuevo Nombre de Usuario:</label>
+                  <input type="text" id="username" name="username" value="${data.username}" required>
+                </td>
+                <td>
+                  <button type="button" id="submitUpdate">Actualizar</button>
+                  <button type="button" class="rojo" id="cancelarUpdate">Cancelar</button>
+                </td>
+              </tr>
+            </tbody>
+          </table>
+        `
+
+        const submitUpdateButton = updateForm.querySelector('#submitUpdate')
+        const cancelarUpdateButton = updateForm.querySelector('#cancelarUpdate')
+
+        submitUpdateButton.addEventListener('click', () => {
+          const updatedData = {
+            username: updateForm.querySelector('#username').value
+          }
+
+          const confirmUpdate = window.confirm(`¿Estás seguro de que deseas actualizar el nombre de usuario: ${data.username}?`)
+
+          if (confirmUpdate) {
+            fetch(`/users/${userId}`, {
+              method: 'PATCH',
+              headers: {
+                'Content-Type': 'application/json'
+              },
+              body: JSON.stringify(updatedData)
+            })
+              .then(response => response.json())
+              .then(patchData => {
+                if (patchData.status === 'ok') {
+                  window.alert('Nombre de usuario actualizado correctamente.')
+
+                  updateFormContainer.removeChild(updateForm)
+
+                  window.location.reload()
+                } else {
+                  console.error('Error al actualizar:', patchData.message)
+                }
+              })
+              .catch(error => {
+                console.error('Error al actualizar:', error)
+              })
+          }
+        })
+
+        cancelarUpdateButton.addEventListener('click', () => {
+          updateFormContainer.removeChild(updateForm)
+        })
+
+        updateFormContainer.innerHTML = ''
+        updateFormContainer.appendChild(updateForm)
+      })
+
+      usernameCell.appendChild(updateUsernameButton)
       row.appendChild(usernameCell)
+
+      // EMAIL
 
       const emailCell = document.createElement('td')
       emailCell.textContent = data.email
+
+      // Botón de Actualizar Email
+      const updateEmailButton = document.createElement('img')
+
+      updateEmailButton.src = '/img/editar.png'
+      updateEmailButton.alt = 'Actualizar Email'
+      updateEmailButton.className = 'chiquito separado'
+
+      // Funcionalidad Botón Actualizar Username
+      updateEmailButton.addEventListener('click', () => {
+        const updateForm = document.createElement('form')
+        const updateFormContainer = document.querySelector('.updateFormContainer')
+
+        updateForm.innerHTML = `
+            <table>
+            <thead>
+              <tr>
+                <th>Email</th>
+                <th></th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <td>
+                  <label for="email">Nuevo Email:</label>
+                  <input type="text" id="email" name="email" value="${data.email}" required>
+                </td>
+                <td>
+                  <button type="button" id="submitUpdate">Actualizar</button>
+                  <button type="button" class="rojo" id="cancelarUpdate">Cancelar</button>
+                </td>
+              </tr>
+            </tbody>
+          </table>
+        `
+
+        const submitUpdateButton = updateForm.querySelector('#submitUpdate')
+        const cancelarUpdateButton = updateForm.querySelector('#cancelarUpdate')
+
+        submitUpdateButton.addEventListener('click', () => {
+          const updatedData = {
+            email: updateForm.querySelector('#email').value
+          }
+
+          const confirmUpdate = window.confirm(`¿Estás seguro de que deseas actualizar el email: ${data.email}?`)
+
+          if (confirmUpdate) {
+            fetch(`/users/${userId}`, {
+              method: 'PATCH',
+              headers: {
+                'Content-Type': 'application/json'
+              },
+              body: JSON.stringify(updatedData)
+            })
+              .then(response => response.json())
+              .then(patchData => {
+                if (patchData.status === 'ok') {
+                  window.alert('Email actualizado correctamente.')
+
+                  updateFormContainer.removeChild(updateForm)
+
+                  window.location.reload()
+                } else {
+                  console.error('Error al actualizar:', patchData.message)
+                }
+              })
+              .catch(error => {
+                console.error('Error al actualizar:', error)
+              })
+          }
+        })
+
+        cancelarUpdateButton.addEventListener('click', () => {
+          updateFormContainer.removeChild(updateForm)
+        })
+
+        updateFormContainer.innerHTML = ''
+        updateFormContainer.appendChild(updateForm)
+      })
+
+      emailCell.appendChild(updateEmailButton)
+
       row.appendChild(emailCell)
 
+      // CONTRASEÑA
+
       const passwordCell = document.createElement('td')
-      passwordCell.textContent = data.password
+      passwordCell.textContent = '*****************'
+
+      const changePasswordButton = document.createElement('img')
+      changePasswordButton.src = '/img/editar.png'
+      changePasswordButton.alt = 'Actualizar Contraseña'
+      changePasswordButton.className = 'chiquito separado'
+
+      // Funcionalidad Botón Actualizar
+      changePasswordButton.addEventListener('click', () => {
+        // Crear el formulario de actualización
+        const updateForm = document.createElement('form')
+        const updateFormContainer = document.querySelector('.updateFormContainer')
+
+        updateForm.innerHTML = `
+            <table>
+            <thead>
+              <tr>
+                <th>Contraseña</th>
+                <th>Confirmar Contraseña</th>
+                <th></th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <td>
+                <label for="password">Nueva Contraseña:</label>
+                <input type="password" id="password" name="password" value="*****************" required>
+                </td>
+                <td>
+                  <label for="confirmedPassword">Repetir Contraseña:</label>
+                  <input type="password" id="confirmedPassword" name="confirmedPassword" value="*****************" required>
+                </td>
+                <td>
+                  <button type="button" id="submitUpdate">Actualizar</button>
+                  <button type="button" class="rojo" id="cancelarUpdate">Cancelar</button>
+                </td>
+              </tr>
+            </tbody>
+          </table>
+        `
+
+        const submitUpdateButton = updateForm.querySelector('#submitUpdate')
+        const cancelarUpdateButton = updateForm.querySelector('#cancelarUpdate')
+
+        submitUpdateButton.addEventListener('click', () => {
+          const updatedData = {
+            password: updateForm.querySelector('#password').value,
+            confirmedPassword: updateForm.querySelector('#confirmedPassword').value
+          }
+
+          const confirmUpdate = window.confirm(`¿Estás seguro de que deseas actualizar la contraseña, ${data.username}?`)
+
+          if (confirmUpdate) {
+            fetch(`/users/${userId}`, {
+              method: 'PATCH',
+              headers: {
+                'Content-Type': 'application/json'
+              },
+              body: JSON.stringify(updatedData)
+            })
+              .then(response => response.json())
+              .then(patchData => {
+                if (patchData.status === 'ok') {
+                  window.alert('Contraseña actualizada correctamente.')
+
+                  updateFormContainer.removeChild(updateForm)
+
+                  window.location.reload()
+                } else {
+                  console.error('Error al actualizar:', patchData.message)
+                }
+              })
+              .catch(error => {
+                console.error('Error al actualizar:', error)
+              })
+          }
+        })
+
+        cancelarUpdateButton.addEventListener('click', () => {
+          updateFormContainer.removeChild(updateForm)
+        })
+
+        updateFormContainer.innerHTML = ''
+        updateFormContainer.appendChild(updateForm)
+      })
+
+      passwordCell.appendChild(changePasswordButton)
       row.appendChild(passwordCell)
 
       const tipoUsuarioCell = document.createElement('td')
       tipoUsuarioCell.textContent = data.tipoUsuario
       row.appendChild(tipoUsuarioCell)
 
-      // Botones de Actualizar y Eliminar
+      // Botón de Eliminar
       const buttonsCell = document.createElement('td')
-      const updateButton = document.createElement('img')
       const deleteButton = document.createElement('img')
-
-      updateButton.src = '/img/editar.png'
-      updateButton.alt = 'Actualizar Usuario'
-      updateButton.className = 'chiquito'
       deleteButton.src = '/img/eliminar.png'
       deleteButton.alt = 'Eliminar Usuario'
       deleteButton.className = 'chiquito'
-
-      // Funcionalidad Botón Actualizar
-      updateButton.addEventListener('click', () => {
-        window.location.href = `/users/update/${userId}`
-      })
 
       // Funcionalidad Botón Eliminar
       deleteButton.addEventListener('click', () => {
@@ -55,7 +300,7 @@ fetch(`/users/details/${userId}`)
             .then(response => response.json())
             .then(deleteData => {
               if (deleteData.status === 'ok') {
-                window.alert('Usuario eliminado exitosamente.')
+                window.alert('Usuario eliminado correctamente.')
 
                 window.location.href = '/home'
               } else {
@@ -68,7 +313,6 @@ fetch(`/users/details/${userId}`)
         }
       })
 
-      buttonsCell.appendChild(updateButton)
       buttonsCell.appendChild(deleteButton)
       row.appendChild(buttonsCell)
 

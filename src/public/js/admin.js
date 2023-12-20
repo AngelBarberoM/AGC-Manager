cc426c8371ec3f2faa9ed4519a1a5471dd5d0d4f
Ángel Barbero Mellado <i82bamea@uco.es>
2023-12-20 18:44:20 +0100

    Cambios en estructuras y css

---
+++ src/public/js/admin.js
@@ -0,0 +1,5 @@
+document.addEventListener('DOMContentLoaded', function () {
+  document.getElementById('redirect').addEventListener('click', function () {
+    window.location.href = '/registerUser'
+  })
+})
